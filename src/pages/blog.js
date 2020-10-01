15027fe3592ae5883882c6a86df6bfeb2296bfd3
18BCS4595A <61505653+18BCS4595A@users.noreply.github.com>
2020-10-01 22:08:52 +0530

    added comments to blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Helmet from "react-helmet";
 import styles from "./blog.module.css";
 import Layout from "../components/layout";
@@ -7,10 +6,17 @@ import ArticlePreview from "../components/article-preview";
 import useSiteMetaData from "../hooks/use-sitemetadata";
 import useContentfulPost from "../hooks/use-contentfulpost";
 
+
+/* this is a blog index page */
+
 const BlogIndex = () => {
+  /* extracting the title from a customhook for post details */
+  
   const { title } = useSiteMetaData();
   const posts = useContentfulPost();
-
+ 
+  /* rendering out the post component on the basis of data recieved */
+  
   return (
     <Layout>
       <div style={{ background: "#fff" }}>
@@ -19,9 +25,11 @@ const BlogIndex = () => {
         <div className="wrapper">
           <h2 className="section-headline">Recent articles</h2>
           <ul className="article-list">
+    {/* mapping through posts array */}
             {posts.map(({ post }) => {
               return (
                 <li key={post.slug}>
+                {/* rendering all the article preview component for more details ArticlePreview component will render the individual post from posts array */}
                   <ArticlePreview article={post} />
                 </li>
               );
@@ -33,4 +41,5 @@ const BlogIndex = () => {
   );
 };
 
+/* exporting the component */
 export default BlogIndex;
